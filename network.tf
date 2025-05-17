f0f707d994d9943bd62c6a4a619ac0ca6912db60
Ankit Pramanik <59945244+ankit98040@users.noreply.github.com>
2025-05-17 16:27:28 +0530

    Update network.tf

--- network.tf
+++ network.tf
@@ -22,7 +22,7 @@ resource "azurerm_virtual_network" "example" {
 }
 
 resource "azurerm_network_security_group" "example" {
-  name                = "{var.prefix}-security-group"
+  name                = "${var.prefix}-security-group"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
 }
